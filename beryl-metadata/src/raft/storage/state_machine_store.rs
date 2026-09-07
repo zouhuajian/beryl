0344457bd4c154ae998e29cbc1380d881c4f761a
Jay.H.Zou <zouhuajianclever@gmail.com>
2026-09-07 10:14:06 +0800

    refactor(metadata): drop unused snapshot state machine reference

--- beryl-metadata/src/raft/storage/state_machine_store.rs
+++ beryl-metadata/src/raft/storage/state_machine_store.rs
@@ -182,7 +182,6 @@ impl RaftStateMachine<MetadataRaftTypeConfig> for StateMachineStorage {
     async fn get_snapshot_builder(&mut self) -> Self::SnapshotBuilder {
         AppSnapshotBuilder {
             storage: Arc::clone(&self.storage),
-            _state_machine: Arc::clone(&self.state_machine),
             _state: Arc::clone(&self.state),
             storage_task: self.storage_task.clone(),
         }
@@ -296,7 +295,6 @@ impl RaftStateMachine<MetadataRaftTypeConfig> for StateMachineStorage {
 /// Snapshot builder for Raft.
 pub(crate) struct AppSnapshotBuilder {
     storage: Arc<RocksDBStorage>,
-    _state_machine: Arc<AppRaftStateMachine>,
     _state: Arc<RwLock<AppMetadataRaftState>>,
     // Detached builders must release their storage owners before shutdown completes.
     storage_task: TaskTrackerToken,
